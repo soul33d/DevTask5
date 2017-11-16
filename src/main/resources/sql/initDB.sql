e71cbafc2977e67b02632229235ff88f3d897409
vitalik <soul33d@gmail.com>
2017-11-17 00:27:07 +0200

    product can't be created without manufacturer_id

--- src/main/resources/sql/initDB.sql
+++ src/main/resources/sql/initDB.sql
@@ -6,10 +6,10 @@ CREATE TABLE manufacturers (
   name VARCHAR(50)
 );
 CREATE TABLE products (
-  id              BINARY(16) PRIMARY KEY NOT NULL ,
-  name            VARCHAR(50) NOT NULL,
-  price           DECIMAL     NOT NULL,
-  manufacturer_id BINARY(16),
+  id              BINARY(16) PRIMARY KEY NOT NULL,
+  name            VARCHAR(50)            NOT NULL,
+  price           DECIMAL                NOT NULL,
+  manufacturer_id BINARY(16)             NOT NULL,
   FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
