52f51b67ea30f360c3e5e337cc816e78ccf7a6fd
vitalik <soul33d@gmail.com>
2017-11-21 01:02:41 +0200

    added tables roles, users and basic content

--- src/main/resources/sql/initDB.sql
+++ src/main/resources/sql/initDB.sql
@@ -1,16 +1,31 @@
 CREATE SCHEMA homework5;
 USE homework5;
 
-CREATE TABLE manufacturers (
+CREATE TABLE IF NOT EXISTS manufacturers (
   id   BINARY(16) PRIMARY KEY NOT NULL,
   name VARCHAR(50)
 );
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
   id              BINARY(16) PRIMARY KEY NOT NULL,
   name            VARCHAR(50)            NOT NULL,
-  price           DECIMAL(18,2)                NOT NULL,
+  price           DECIMAL(18, 2)         NOT NULL,
   manufacturer_id BINARY(16)             NOT NULL,
   FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
-)
+);
+CREATE TABLE IF NOT EXISTS roles (
+  id   BINARY(16) PRIMARY KEY NOT NULL,
+  name VARCHAR(50)            NOT NULL
+);
+CREATE TABLE IF NOT EXISTS users (
+  id         BINARY(16) PRIMARY KEY NOT NULL,
+  first_name VARCHAR(50)            NOT NULL,
+  last_name  VARCHAR(50)            NOT NULL,
+  email      VARCHAR(50)            NOT NULL,
+  password   VARCHAR(255)           NOT NULL,
+  role_id    BINARY(16)             NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES roles (id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
--- src/main/resources/sql/populateDB.sql
+++ src/main/resources/sql/populateDB.sql
@@ -1,14 +1,17 @@
 USE homework5;
 
-INSERT INTO manufacturers (id, name) VALUES (1, 'ZAZ');
-INSERT INTO manufacturers (id, name) VALUES (2, 'Mercedes');
+INSERT INTO manufacturers VALUES (1, 'ZAZ');
+INSERT INTO manufacturers VALUES (2, 'Mercedes');
 
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (1, 'VIDA', 10000, 1);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (2, 'Sens', 7000, 1);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (3, 'Lanos Cargo', 9000, 1);
+INSERT INTO products VALUES (1, 'VIDA', 10000, 1);
+INSERT INTO products VALUES (2, 'Sens', 7000, 1);
+INSERT INTO products VALUES (3, 'Lanos Cargo', 9000, 1);
 
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (4, 'Mercedes-AMG C 43 4MATIC', 50000, 2);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (5, 'Mercedes-AMG C 63', 750000, 2);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (6, 'Mercedes-Benz E 220 d', 450000, 2);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (7, 'Mercedes-AMG E 43 Sport', 750000, 2);
-INSERT INTO products (id, name, price, manufacturer_id) VALUES (8, 'MERCEDES-MAYBACH S 560 4MATIC', 1800000, 2);
+INSERT INTO products VALUES (4, 'Mercedes-AMG C 43 4MATIC', 50000, 2);
+INSERT INTO products VALUES (5, 'Mercedes-AMG C 63', 750000, 2);
+INSERT INTO products VALUES (6, 'Mercedes-Benz E 220 d', 450000, 2);
+INSERT INTO products VALUES (7, 'Mercedes-AMG E 43 Sport', 750000, 2);
+INSERT INTO products VALUES (8, 'MERCEDES-MAYBACH S 560 4MATIC', 1800000, 2);
+
+INSERT INTO roles VALUES (1, 'Admin');
+INSERT INTO roles VALUES (2, 'User');
