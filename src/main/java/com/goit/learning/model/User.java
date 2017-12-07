f6ceefa0c92c0d097b9ebedf50b902d71d4e3a20
vitalik <soul33d@gmail.com>
2017-12-07 10:24:50 +0200

    roles fetch type = EAGER

--- src/main/java/com/goit/learning/model/User.java
+++ src/main/java/com/goit/learning/model/User.java
@@ -29,7 +29,7 @@ public class User implements Comparable<User> {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
