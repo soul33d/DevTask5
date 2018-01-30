95f9c33faf1c6ca485aaa3eb99196989156e07e9
vitalik <soul33d@gmail.com>
2018-01-30 02:32:22 +0200

    constructors

--- src/main/java/com/goit/learning/model/User.java
+++ src/main/java/com/goit/learning/model/User.java
@@ -32,6 +32,17 @@ public class User implements Comparable<User> {
     @Column(name = "role")
     private Role role;
 
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     public UUID getId() {
         return id;
     }
