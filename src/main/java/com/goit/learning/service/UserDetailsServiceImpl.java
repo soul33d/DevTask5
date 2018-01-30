2b7b23f965d5909d888fc799b24ec3249708127d
vitalik <soul33d@gmail.com>
2018-01-30 02:33:11 +0200

    repository changed to service

--- src/main/java/com/goit/learning/service/UserDetailsServiceImpl.java
+++ src/main/java/com/goit/learning/service/UserDetailsServiceImpl.java
@@ -1,8 +1,7 @@
-package com.goit.learning.dao;
+package com.goit.learning.service;
 
 import com.goit.learning.model.User;
 import com.goit.learning.model.UserDetailsImpl;
-import com.goit.learning.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
+        User user = userService.findByEmail(username);
         if (user == null) throw new UsernameNotFoundException("There is no user with email: " + username);
         return new UserDetailsImpl(user);
     }
