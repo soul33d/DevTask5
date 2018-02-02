e9b6777486e75fbc79c2c72731516f7314887ba6
vitalik <soul33d@gmail.com>
2018-02-02 11:58:15 +0200

    prod properties and configs

--- src/main/java/com/goit/learning/config/WebAppInitializer.java
+++ src/main/java/com/goit/learning/config/WebAppInitializer.java
@@ -9,10 +9,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import static java.lang.System.getenv;
+
 public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        String spring_profile = getenv().containsKey("SPRING_PROFILE") ? getenv("SPRING_PROFILE") : "default";
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.getEnvironment().setActiveProfiles(spring_profile);
         rootContext.register(DataConfig.class);
         rootContext.register(SecurityConfig.class);
         servletContext.addListener(new ContextLoaderListener(rootContext));
@@ -20,7 +24,8 @@ public class WebAppInitializer implements WebApplicationInitializer {
         AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
         dispatcherServlet.register(WebConfig.class);
 
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet
+                ("dispatcher", new DispatcherServlet(dispatcherServlet));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
     }
